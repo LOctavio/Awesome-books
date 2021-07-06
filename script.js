e6a72b425078ec5318eede684494530579ec7d39
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-07-06 15:48:00 -0700

    refactoring

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var bookLists = [];
-localStorage.setItem('bookslist', JSON.stringify(bookLists));
-document.getElementById("name").innerHTML = JSON.stringify(bookLists);
 
-function AddNewBook() {
-  const newbook = {
-    title: document.getElementById("title").value,
-    author: document.getElementById("author").value
-  }
-  bookLists.push(newbook);
-  localStorage.setItem('bookslist', JSON.stringify(bookLists));
-  document.getElementById("name").innerHTML = JSON.stringify(bookLists);
+const bookList = []
+function Book(title, author) {
+  this.title = title
+  this.author = author
+}
+
+
+
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault()
+  const title = document.getElementById("title").value
+  const author = document.getElementById("book-author").value
+
+  let div = document.createElement('div');
+  div.innerHTML = `
+  <p>${title}</p>
+  <p>${author}</p>`
+ let list = document.getElementById("list")
+  const book = new Book(title, author)
+  bookList.push(book)
+  list.appendChild(div)
+saveLocalStorage()
+console.log(book)
+})
+
+const saveLocalStorage = () => {
+  window.localStorage.setItem('books', JSON.stringify(bookList));
 }
 
 
