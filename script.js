701e8aaeb262e5257eab938ce878c12c270b88b7
LOctavio <luis_ram1997@hotmail.com>
2021-07-06 11:55:43 -0500

    add remove function(incomplete)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-const bookList = []
+let bookList = []
+let title;
+let author;
 function Book(title, author) {
   this.title = title
   this.author = author
@@ -15,14 +17,30 @@ document.getElementById("form").addEventListener("submit", (e) => {
   let div = document.createElement('div');
   div.innerHTML = `
   <p>${title}</p>
-  <p>${author}</p>`
+  <p>${author}</p>
+  <a onclick = "remove('${title, author}')" id="delete">Remove Book</a>`
  let list = document.getElementById("list")
   const book = new Book(title, author)
   bookList.push(book)
   list.appendChild(div)
-saveLocalStorage()
-console.log(book)
+  saveLocalStorage()
+  console.log(book)
 })
+
+function filterByTitle(item){
+  if( item.title===this.title && item.author===this.author) {
+    return true
+  }else {
+    return false
+  }
+}
+function remove(title, author){
+  this.title=title
+  this.author=author
+  bookList = bookList.filter(filterByTitle)
+  console.log(bookList)
+}
+
 
 const saveLocalStorage = () => {
   window.localStorage.setItem('books', JSON.stringify(bookList));
