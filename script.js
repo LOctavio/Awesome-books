e57c19c17622053050b8351b0cc93e1f28c07030
LOctavio <luis_ram1997@hotmail.com>
2021-07-07 17:16:29 -0500

    add method to load list and refactor code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -5,49 +7,63 @@ class Book {
   }
 }
 let books = [];
+
+window.onload = function() {
+  for (let i = 0; i < localStorage.length; i += 1) {
+    title = localStorage.key(i);
+    author = localStorage.getItem(localStorage.key(i));
+    let book = new Book(title, author);
+    UI.bookDisplay(title, author);
+    books.push(book);
+ }
+};
 class Store {
   static saveLocalStorage (title, author) {
     window.localStorage.setItem(title, author);
-    for (let i = 0; i < localStorage.length; i += 1) {
-      title = localStorage.key(i);
-      author = localStorage.getItem(localStorage.key(i));
-      UI.bookDisplay(title, author);
-      books.push(book);
-   }
   };
   
 }
 class UI {
-  static bookDisplay() {
+  constructor() {
     
-    books.forEach((book) => UI.addBook(book));
+  }
+  
+  static bookDisplay(title, author) {
+    
+      const bookList = document.getElementById('book-list');
+
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${title}</td>
+        <td>${author}</td>
+        <td><button class="delete">Remove Book</button></td>
+      `;
+      bookList.appendChild(row);
+    
   }
 
+  
+
   static addBook(book) {
-    const bookList = document.getElementById('book-list');
-
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td><button class="delete">Remove Book</button></td>
-    `;
+    this.bookDisplay(book.title, book.author);
     //document.getElementById("form").reset()
    
     bookList.appendChild(row);
     Store.saveLocalStorage(book.title, book.author)
+    console.log(books);
   }
   static remove(el) {
     if(el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
+      localStorage.removeItem(el.parentElement.parentElement.firstChild.nextSibling.textContent);
     }
   }
 
 }
 
 
-document.addEventListener('DOMContentLoaded', UI.bookDisplay);
+
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.getElementById('title').value;
