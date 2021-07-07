c0708cc3dd3096813c3dedd30e25c7463d1825db
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-07-06 23:41:01 -0700

    fix linters

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-let bookList = []
+let bookList = [];
 let book;
 let title;
 let author;
-for (var i = 0; i<localStorage.length; i++) {
+for (let i = 0; i < localStorage.length; i++) {
   title = localStorage.key(i);
   author = localStorage.getItem(localStorage.key(i));
   displayBook(title, author);
@@ -18,24 +17,27 @@ function displayBook(title, author) {
   <p>${title}</p>
   <p>${author}</p>
   <button onclick = "remove('${title}', '${author}', this)" class="delete">Remove Book</button>`
-  list.appendChild(div)
+  list.appendChild(div);
+
 }
 
 function Book(title, author) {
   this.title = title
   this.author = author
+
 }
 
 
 
 document.getElementById("form").addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   title = document.getElementById("title").value
   author = document.getElementById("book-author").value
-  book = new Book(title, author)
-  bookList.push(book)
-  saveLocalStorage(title, author)
-  displayBook(title, author)
+  book = new Book(title, author);
+  bookList.push(book);
+  saveLocalStorage(title, author);
+  displayBook(title, author);
+
 })
 
 function filterByTitle(item){
@@ -45,10 +47,10 @@ function remove(title, author, e){
   this.title=title
   this.author=author
   e.parentNode.parentNode.removeChild(e.parentNode);
-  bookList = bookList.filter(filterByTitle)
-  localStorage.removeItem(title)
-}
+  bookList = bookList.filter(filterByTitle);
+  localStorage.removeItem(title);
 
+}
 
 const saveLocalStorage = (title, author) => {
   window.localStorage.setItem(title, author);
