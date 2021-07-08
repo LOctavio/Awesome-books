d5fcec64c5f1accb2042c9a18bb2db840717c672
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-07-07 23:40:33 -0700

    fix ESlint

--- script.js
+++ script.js
@@ -1,77 +1,58 @@
-
-
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
-let books = [];
+const books = [];
+let title;
+let author;
 
-window.onload = function() {
-  for (let i = 0; i < localStorage.length; i += 1) {
-    title = localStorage.key(i);
-    author = localStorage.getItem(localStorage.key(i));
-    let book = new Book(title, author);
-    UI.bookDisplay(title, author);
-    books.push(book);
- }
-};
-class Store {
-  static saveLocalStorage (title, author) {
-    window.localStorage.setItem(title, author);
-  };
-  
-}
 class UI {
-  constructor() {
-    
-  }
-  
   static bookDisplay(title, author) {
-    
-      const bookList = document.getElementById('book-list');
+    const bookList = document.getElementById('book-list');
 
-      const row = document.createElement('tr');
+    const row = document.createElement('tr');
 
-      row.innerHTML = `
+    row.innerHTML = `
         <td>${title}</td>
         <td>${author}</td>
         <td><button class="delete">Remove Book</button></td>
       `;
-      bookList.appendChild(row);
-    
+    bookList.appendChild(row);
+    document.getElementById('book-form').reset();
   }
-
-  
 
   static addBook(book) {
     this.bookDisplay(book.title, book.author);
-    //document.getElementById("form").reset()
-   
-    Store.saveLocalStorage(book.title, book.author)
-    console.log(books);
+    window.localStorage.setItem(book.title, book.author);
   }
+
   static remove(el) {
-    if(el.classList.contains('delete')) {
+    if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
       localStorage.removeItem(el.parentElement.parentElement.firstChild.nextSibling.textContent);
     }
   }
-
 }
-
-
+window.onload = function () {
+  for (let i = 0; i < localStorage.length; i += 1) {
+    title = localStorage.key(i);
+    author = localStorage.getItem(localStorage.key(i));
+    const book = new Book(title, author);
+    UI.bookDisplay(title, author);
+    books.push(book);
+  }
+};
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-  
-    const book = new Book(title, author);
-    UI.addBook(book);
-})
+
+  const book = new Book(title, author);
+  UI.addBook(book);
+});
 document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.remove(e.target);
 });
-
